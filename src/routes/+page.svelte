44b2132ef63e3d0a0d1b05303f95b8ae27b4e306
Gus Ruben <gusruben@protonmail.com>
2023-01-28 19:57:26 -0500

    Add Public Sans + import bulma vars into vars.scss

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,32 +1,41 @@
 <script lang="ts">
-	import { onMount } from "svelte";
-	import { supabase } from "./supabase";
-	import type { AuthSession } from "@supabase/supabase-js";
-	import Login from "./login.svelte";
-	import Notes from "./notes.svelte";
-	import { goto } from "$app/navigation";
-	import "../style/landing.scss";
+    import { onMount } from "svelte";
+    import { supabase } from "./supabase";
+    import type { AuthSession } from "@supabase/supabase-js";
+    import Login from "./login.svelte";
+    import Notes from "./notes.svelte";
+    import { goto } from "$app/navigation";
+    import "@fontsource/public-sans";
+    import "../style/landing.scss";
 
-	let session: AuthSession | null;
-	let loaded: boolean = false;
+    let session: AuthSession | null;
+    let loaded: boolean = false;
 
-	onMount(() => {
-		supabase.auth.getSession().then(({ data }) => {
-			session = data.session;
+    onMount(() => {
+        supabase.auth.getSession().then(({ data }) => {
+            session = data.session;
 
-			loaded = true;
-		});
+            loaded = true;
+        });
 
-		supabase.auth.onAuthStateChange((_event, _session) => {
-			session = _session;
-		});
-	});
+        supabase.auth.onAuthStateChange((_event, _session) => {
+            session = _session;
+        });
+    });
 </script>
 
+<nav class="navbar">
+    <div class="navbar-brand">
+        <div class="navbar-item">
+            <button class="button is-primary" />
+        </div>
+    </div>
+</nav>
+
 {#if session}
-	<button on:click={() => goto("/account")}>Account</button>
-	<button on:click={() => goto("/upload")}>Upload</button>
-	<Notes {session} />
+    <button on:click={() => goto("/account")}>Account</button>
+    <button on:click={() => goto("/upload")}>Upload</button>
+    <Notes {session} />
 {:else if loaded}
-	<Login />
+    <Login />
 {/if}
