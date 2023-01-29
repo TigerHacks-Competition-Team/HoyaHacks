197ba6facc517442cce1c8aacc9b5e24a44e5ae9
Gus Ruben <gusruben@protonmail.com>
2023-01-29 01:03:49 -0500

    Finish landing page + flesh out SCSS layout more

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,7 @@
     import Notes from "./notes.svelte";
     import { goto } from "$app/navigation";
     import "@fontsource/public-sans";
-    import "@fortawesome/fontawesome-free/css/all.css";
+    import "@fortawesome/fontawesome-free/css/all.min.css";
     import "../style/landing.scss";
 
     let session: AuthSession | null;
@@ -43,10 +43,94 @@
     </div>
 </nav>
 
-{#if session}
-    <button on:click={() => goto("/account")}>Account</button>
-    <button on:click={() => goto("/upload")}>Upload</button>
-    <Notes {session} />
-{:else if loaded}
-    <Login />
-{/if}
+<div class="section" id="section-1">
+    <div class="columns is-desktop">
+        <div class="column">
+            <div class="info-text">
+                <h1 class="title is-1">
+                    Blah Blah <span class="emphasis">Better.</span>
+                </h1>
+                <h3 class="subtitle is-3">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                </h3>
+                <button class="button is-primary">Get Started</button>
+                <button class="button">More Info</button>
+            </div>
+        </div>
+        <div class="column has-text-right">
+            <img src="art1.svg" alt="" class="art is-first" />
+        </div>
+    </div>
+</div>
+
+<div class="section" id="section-2">
+    <div class="slant-bg" />
+    <div class="columns is-desktop">
+        <div class="column">
+            <img src="art2.svg" alt="" class="art is-second" />
+        </div>
+        <div class="column">
+            <div class="info-text">
+                <h1 class="title is-1">
+                    Something <span class="emphasis">Easier.</span>
+                </h1>
+                <h3 class="subtitle is-3">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                </h3>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="section" id="section-3">
+    <div id="login-form">
+        <h1 class="title is-1">Get Started</h1>
+        <button class="button is-large">
+            <span class="icon fake-icon is-medium">
+                <img src="google.svg" alt="" />
+            </span>
+            <span>Sign up with Google</span>
+        </button>
+        <span class="divider">Already a member?</span>
+        <button class="button is-large">
+            <span class="icon fake-icon is-medium">
+                <img src="google.svg" alt="" />
+            </span>
+            <span>Log in</span>
+        </button>
+    </div>
+
+    <div class="footer" id="footer">
+        <div class="content has-text-centered">
+            <p>
+                ProjectName by
+                <a href="https://github.com/TigerHacks-Competition-Team/">
+                    PantheraHacks / TigerHacks.
+                </a>
+                This app was built using <a href="https://svelte.dev">Svelte</a>
+                and <a href="https://bulma.io">Bulma.</a> You can view the
+                source code
+                <a
+                    href="https://github.com/TigerHacks-Competition-Team/HoyaHacks"
+                    >here.</a
+                >
+            </p>
+        </div>
+    </div>
+</div>
+
+<div class="is-hidden">
+    {#if session}
+        <button on:click={() => goto("/account")}>Account</button>
+        <button on:click={() => goto("/upload")}>Upload</button>
+        <Notes {session} />
+    {:else if loaded}
+        <Login />
+    {/if}
+</div>
